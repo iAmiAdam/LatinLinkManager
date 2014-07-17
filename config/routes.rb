f748d39a0948ed5dcb2583a3941b406c844d548f
Adam Smith <adam.smith18@ntlworld.com>
2014-07-17 12:41:37 +0100

    Controller made

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 LatinLinkManager::Application.routes.draw do
   resources :users
+  get "static_pages/home"
   root 'static_pages#home'
   
   # The priority is based upon order of creation: first created -> highest priority.
