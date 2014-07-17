0a8963924e7686a1067a3ae871cdd514d5ad7968
Adam Smith <adam.smith18@ntlworld.com>
2014-07-17 12:31:32 +0100

    Show user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 LatinLinkManager::Application.routes.draw do
-  get "users/new"
+  resources :users
+  root 'static_pages#home'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140717111514) do
+ActiveRecord::Schema.define(version: 20140717112242) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -21,5 +21,7 @@ ActiveRecord::Schema.define(version: 20140717111514) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
