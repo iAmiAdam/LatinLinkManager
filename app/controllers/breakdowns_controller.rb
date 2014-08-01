63ee590c06dbc47853915bbdc38eeb0c8c536f4f
Adam Smith <adam.smith18@ntlworld.com>
2014-08-01 14:55:38 +0100

    Percentages breakdown finished

--- app/controllers/breakdowns_controller.rb
+++ app/controllers/breakdowns_controller.rb
@@ -4,7 +4,18 @@ class BreakdownsController < ApplicationController
 	end
 
 	def create 
+		@project = Project.find(new_breakdown_params[:project_id])
+		@breakdown = @project.breakdowns.build(new_breakdown_params)
+		@breakdown.sort = 1
+		@breakdown.save
+		redirect_to @project
+	end
 
+	def update
+		@breakdown = Breakdown.find(params[:id])
+		@breakdown.update_attributes(new_breakdown_params)
+		@project = Project.find(new_breakdown_params[:project_id])
+		redirect_to @project
 	end
 
 	def destroy
@@ -14,6 +25,6 @@ class BreakdownsController < ApplicationController
 	private
 
 		def new_breakdown_params
-			params.require(:breakdown).permit(:project_id, :translated, :context, :repetition, :format, :hundred, :ninety_five, :eighty_five, :seventy_five, :fifty, :new, :total)
+			params.require(:breakdown).permit(:project_id, :translated, :context, :repetition, :format, :hundred, :ninety_five, :eighty_five, :seventy_five, :fifty, :nomatch, :total)
 		end	
 end
