bb9a32dbb4bdf381b3da1147890ed5d184513919
Adam Smith <adam.smith18@ntlworld.com>
2014-08-03 09:05:20 +0100

    Updated Mailer

--- app/mailers/project_mailer.rb
+++ app/mailers/project_mailer.rb
@@ -1,7 +1,7 @@
 class ProjectMailer < ActionMailer::Base
   default from: current_user.email
 
-  def project_email(translator)
+  def project_email(translator, project)
   	@translator = translator
   	mail(to: @translator.email, subject: "")
   end
