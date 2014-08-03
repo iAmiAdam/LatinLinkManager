6493d821451c8b78449ec027c9b3dcc8f0248795
Adam Smith <adam.smith18@ntlworld.com>
2014-08-03 09:16:33 +0100

    Updated static pages controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,12 @@
 class StaticPagesController < ApplicationController
+	before_action :signed_in_user
+
 	def home
 	end
+
+	private
+
+		def signed_in_user
+  			redirect_to signin_url, notice: "Please Sign in." unless signed_in?
+  		end
 end
