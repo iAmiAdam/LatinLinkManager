e815e849ab47dfb08ee89be0e339758837cd2f05
Adam Smith <adam.smith18@ntlworld.com>
2014-08-06 09:31:41 +0100

    Variables added to project mailer

--- app/mailers/project_mailer.rb
+++ app/mailers/project_mailer.rb
@@ -3,6 +3,19 @@ class ProjectMailer < ActionMailer::Base
 
   def project_email(translator, project)
   	@translator = translator
+  	@project = project
+
+  	@words = Breakdown.new
+	@percents = Breakdown.new
+
+	@breakdowns = @project.breakdowns
+	@breakdowns.each do |b|
+		if b.sort == 1
+			@words = b
+		else
+			@percents = b
+		end
+	end
   	email_with_name = "#{@translator.name} <#{@translator.email}>"
   	mail(to: email_with_name, subject: "Handoff #{}, request, deadline")
   end
