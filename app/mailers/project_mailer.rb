dab13a5cbd3a9a7139af08228218754f65841e67
Adam Smith <adam.smith18@ntlworld.com>
2014-08-06 12:55:33 +0100

    Updated project email table

--- app/mailers/project_mailer.rb
+++ app/mailers/project_mailer.rb
@@ -16,6 +16,13 @@ class ProjectMailer < ActionMailer::Base
 			@percents = b
 		end
 	end
+	@assignment = Assignment.where(:translator_id => @translator.id, :project_id => @project.id)
+	@rate = @assignment.rate
+	@link = Link.where(:translator_id => @translator.id, :project_id => @project.id)
+	@order = Order.find(@link.order_id)
+	@total = @order.value
+
+
   	email_with_name = "#{@translator.name} <#{@translator.email}>"
   	mail(to: email_with_name, subject: "Handoff number | request | #{@project.deadline} #{project.time.strftime("%l:%M")}")
   end
